780f7c23855d34b8f24d28d1a87f1a26c58b3e55
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-05-15 16:58:56 +0700

    Edit heading font size for small screen

--- src/components/General/Heading/Heading.tsx
+++ src/components/General/Heading/Heading.tsx
@@ -6,7 +6,7 @@ type HeadingProps = {
 function Heading({ sectionName, subName }: HeadingProps) {
   return (
     <div className="relative flex text-center mb-5">
-      <h2 className="text-gray-200 max-lg:text-[calc(1.95rem+8.4vw)] text-[calc(1.95rem+6vw)] font-bold opacity-40 uppercase w-full">
+      <h2 className="text-gray-200 max-[400px]:text-[calc(1.95rem+6vw)] max-lg:text-[calc(1.95rem+8.4vw)] text-[calc(1.95rem+6vw)] font-bold opacity-40 uppercase w-full">
         {sectionName}
       </h2>
       <div className="absolute self-center text-4xl font-bold w-full">
