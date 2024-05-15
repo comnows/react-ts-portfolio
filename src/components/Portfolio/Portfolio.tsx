69fc3b655d197c5f15a88c7a332b5828428068a6
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-05-15 16:41:08 +0700

    Edit works list to map from array and Add responsive design in portfolio section

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,39 @@
 import Heading from "../General/Heading/Heading";
-import todoReactPic from "/todo-react.jpg";
+
+type workObject = {
+  id: number;
+  name: string;
+  category: string;
+  image: string;
+  link?: string;
+};
+
+const works: workObject[] = [
+  {
+    id: 1,
+    name: "Todo Web",
+    category: "Full Stack",
+    image: "/todo-react.jpg",
+  },
+  {
+    id: 2,
+    name: "Nowat",
+    category: "Front End",
+    image: "/nowat-shopping-web.jpg",
+  },
+  {
+    id: 3,
+    name: "Nowat",
+    category: "Front End",
+    image: "/nowat-shopping-web.jpg",
+  },
+  {
+    id: 4,
+    name: "Nowat",
+    category: "Front End",
+    image: "/nowat-shopping-web.jpg",
+  },
+] as const;
 
 function Portfolio() {
   return (
@@ -8,72 +42,33 @@ function Portfolio() {
         <div className="container mx-auto">
           <Heading sectionName="Portfolio" subName="My Work" />
           <div className="w-full">
-            <ul className="grid grid-cols-3 gap-5">
-              <li className="">
-                <a href="" className="group/work-link-1 relative">
-                  <img src={todoReactPic} alt="" className="w-full h-full" />
-                  <div className="absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black invisible opacity-0 group-hover/work-link-1:visible group-hover/work-link-1:opacity-70 transition-all duration-300">
-                    <h5 className="text-white text-xl opacity-0 group-hover/work-link-1:opacity-100 transition-all duration-500">
-                      Project Title
-                    </h5>
-                    <div className="text-neutral-100 opacity-0 group-hover/work-link-1:opacity-100 transition-all duration-500">
-                      Category
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="">
-                <a href="" className="group/work-link-2 relative">
-                  <img src={todoReactPic} alt="" className="w-full h-full" />
-                  <div className="absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black invisible opacity-0 group-hover/work-link-2:visible group-hover/work-link-2:opacity-70 transition-all duration-300">
-                    <h5 className="text-white text-xl opacity-0 group-hover/work-link-2:opacity-100 transition-all duration-500">
-                      Project Title
-                    </h5>
-                    <div className="text-neutral-100 opacity-0 group-hover/work-link-2:opacity-100 transition-all duration-500">
-                      Category
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="">
-                <a href="" className="group/work-link-3 relative">
-                  <img src={todoReactPic} alt="" className="w-full h-full" />
-                  <div className="absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black invisible opacity-0 group-hover/work-link-3:visible group-hover/work-link-3:opacity-70 transition-all duration-300">
-                    <h5 className="text-white text-xl opacity-0 group-hover/work-link-3:opacity-100 transition-all duration-500">
-                      Project Title
-                    </h5>
-                    <div className="text-neutral-100 opacity-0 group-hover/work-link-3:opacity-100 transition-all duration-500">
-                      Category
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="">
-                <a href="" className="group/work-link-4 relative">
-                  <img src={todoReactPic} alt="" className="w-full h-full" />
-                  <div className="absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black invisible opacity-0 group-hover/work-link-4:visible group-hover/work-link-4:opacity-70 transition-all duration-300">
-                    <h5 className="text-white text-xl opacity-0 group-hover/work-link-4:opacity-100 transition-all duration-500">
-                      Project Title
-                    </h5>
-                    <div className="text-neutral-100 opacity-0 group-hover/work-link-4:opacity-100 transition-all duration-500">
-                      Category
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="">
-                <a href="" className="group/work-link-5 relative">
-                  <img src={todoReactPic} alt="" className="w-full h-full" />
-                  <div className="absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black invisible opacity-0 group-hover/work-link-5:visible group-hover/work-link-5:opacity-70 transition-all duration-300">
-                    <h5 className="text-white text-xl opacity-0 group-hover/work-link-5:opacity-100 transition-all duration-500">
-                      Project Title
-                    </h5>
-                    <div className="text-neutral-100 opacity-0 group-hover/work-link-5:opacity-100 transition-all duration-500">
-                      Category
-                    </div>
-                  </div>
-                </a>
-              </li>
+            <ul className="grid max-sm:grid-cols-1 max-xl:grid-cols-2 grid-cols-3 gap-5">
+              {works.map((work) => {
+                return (
+                  <li key={work.id} className="">
+                    <a
+                      href={work.link}
+                      className={`group/work-link-${work.id} relative`}
+                    >
+                      <img src={work.image} alt="" className="w-full h-full" />
+                      <div
+                        className={`absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black/70 invisible opacity-0 group-hover/work-link-${work.id}:visible group-hover/work-link-${work.id}:opacity-100 transition-all duration-300`}
+                      >
+                        <h5
+                          className={`text-white text-xl opacity-0 group-hover/work-link-${work.id}:opacity-100 transition-all duration-500`}
+                        >
+                          {work.name}
+                        </h5>
+                        <div
+                          className={`text-neutral-100 opacity-0 group-hover/work-link-${work.id}:opacity-100 transition-all duration-500`}
+                        >
+                          {work.category}
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
