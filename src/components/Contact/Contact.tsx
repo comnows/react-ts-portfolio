644fcad511d1111f0f88e586f9c8dbd405bc2da3
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-05-16 12:10:22 +0700

    Add responsive design to contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,10 +5,10 @@ import Heading from "../General/Heading/Heading";
 function Contact() {
   return (
     <section id="contact">
-      <div className="container mx-auto px-24 py-[72px]">
+      <div className="container mx-auto px-6 lg:px-24 py-[72px]">
         <Heading sectionName="Contact" subName="Get in Touch" />
-        <div className="flex gap-5">
-          <div className="basis-1/4 mr-">
+        <div className="flex max-sm:flex-col gap-5 max-sm:gap-8">
+          <div className="basis-1/4 max-2xl:basis-2/6 max-xl:basis-1/2 max-sm:basis-full max-sm:text-center">
             <h3 className="text-xl font-bold uppercase mb-4">Where am I</h3>
             <div className="mb-6">
               I'm currently living in Bangkok, Thailand
@@ -33,7 +33,7 @@ function Contact() {
                 </span>
               </div>
             </div>
-            <div className="flex gap-4">
+            <div className="flex max-sm:justify-center gap-4">
               <a href="" className="group/facebook">
                 <FaFacebook
                   className="text-gray-600 group-hover/facebook:text-[#3b5998]"
@@ -48,7 +48,7 @@ function Contact() {
               </a>
             </div>
           </div>
-          <div className="basis-3/4">
+          <div className="basis-3/4 max-2xl:basis-4/6 max-lg:basis-1/2 max-sm:basis-full">
             <MessageForm />
           </div>
         </div>
