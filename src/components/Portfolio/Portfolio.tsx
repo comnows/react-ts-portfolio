cb735b1bfbaf5f32ca2fcea78825051332b86450
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-05-20 14:27:43 +0700

    Add work to portfolio section

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -14,24 +14,61 @@ const works: workObject[] = [
     name: "Todo Web",
     category: "Full Stack",
     image: "/todo-react.jpg",
+    link: "http://todolist-774fc.web.app",
   },
   {
     id: 2,
     name: "Nowat",
     category: "Front End",
     image: "/nowat-shopping-web.jpg",
+    link: "https://youtu.be/sNdDc6wF3yM",
   },
   {
     id: 3,
-    name: "Nowat",
+    name: "Calculator React",
     category: "Front End",
-    image: "/nowat-shopping-web.jpg",
+    image: "/calculator-react.jpg",
+    link: "https://youtu.be/sWqwD73u9nM",
   },
   {
     id: 4,
-    name: "Nowat",
-    category: "Front End",
-    image: "/nowat-shopping-web.jpg",
+    name: "Plant Play",
+    category: "Flutter",
+    image: "/plant-play-full.jpg",
+    link: "https://youtu.be/2o48N3CODHU?si=CAQ8zOAGhap1GLla",
+  },
+  {
+    id: 5,
+    name: "Tiny Agent",
+    category: "Game Dev",
+    image: "/tiny-agent.jpg",
+    link: "https://youtu.be/WISVtyGuBGo",
+  },
+  {
+    id: 6,
+    name: "VR FPS Game",
+    category: "Game Dev",
+    image: "/vr-fps-game.jpg",
+    link: "https://youtu.be/EJfr6N9M9-s",
+  },
+  {
+    id: 7,
+    name: "Tangthai in Amazon",
+    category: "Game Dev",
+    image: "/tang-thai-in-amazon.jpg",
+    link: "https://youtu.be/QYhCsCSH4lw",
+  },
+  {
+    id: 8,
+    name: "Pet Planet",
+    category: "Game Dev",
+    image: "/PetPlanet.png",
+  },
+  {
+    id: 9,
+    name: "Blocken",
+    category: "Game Dev",
+    image: "/Blocken.png",
   },
 ] as const;
 
@@ -43,32 +80,35 @@ function Portfolio() {
           <Heading sectionName="Portfolio" subName="My Work" />
           <div className="w-full">
             <ul className="grid max-sm:grid-cols-1 max-xl:grid-cols-2 grid-cols-3 gap-5">
-              {works.map((work) => {
-                return (
-                  <li key={work.id} className="">
-                    <a
-                      href={work.link}
-                      className={`group/work-link-${work.id} relative`}
+              {works.map((work) => (
+                <li key={work.id} className="">
+                  <a
+                    href={work.link}
+                    target="_blank"
+                    className={`group relative`}
+                  >
+                    <img
+                      src={work.image}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
+                    <div
+                      className={`absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black/70 invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all duration-300`}
                     >
-                      <img src={work.image} alt="" className="w-full h-full" />
-                      <div
-                        className={`absolute flex flex-col justify-center text-center top-0 left-0 w-full h-full bg-black/70 invisible opacity-0 group-hover/work-link-${work.id}:visible group-hover/work-link-${work.id}:opacity-100 transition-all duration-300`}
+                      <h5
+                        className={`text-white text-xl opacity-0 group-hover:opacity-100 transition-all duration-500`}
                       >
-                        <h5
-                          className={`text-white text-xl opacity-0 group-hover/work-link-${work.id}:opacity-100 transition-all duration-500`}
-                        >
-                          {work.name}
-                        </h5>
-                        <div
-                          className={`text-neutral-100 opacity-0 group-hover/work-link-${work.id}:opacity-100 transition-all duration-500`}
-                        >
-                          {work.category}
-                        </div>
+                        {work.name}
+                      </h5>
+                      <div
+                        className={`text-neutral-100 opacity-0 group-hover:opacity-100 transition-all duration-500`}
+                      >
+                        {work.category}
                       </div>
-                    </a>
-                  </li>
-                );
-              })}
+                    </div>
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
